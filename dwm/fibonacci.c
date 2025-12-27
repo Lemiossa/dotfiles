813818625dc0a6321db7900e34acfd1bcca23772
Lemiossa <matheuslemedasilva000@gmail.com>
2025-12-27 18:46:31 -0300

    Picom com novas configurações e resolvido outro problema do fibonacci com gaps

--- dwm/fibonacci.c
+++ dwm/fibonacci.c
@@ -1,24 +1,22 @@
-
 void
 fibonacci(Monitor *mon, int s) {
 	unsigned int i, n, nx, ny, nw, nh;
-	unsigned int oe, ie; // outer e inner gaps
+	unsigned int oe, ie;
 	Client *c;
-
+	
 	for(n = 0, c = nexttiled(mon->clients); c; c = nexttiled(c->next), n++);
 	if(n == 0)
 		return;
-
-	// pega gaps direto do monitor
+	
 	oe = ie = enablegaps;
 	if(smartgaps && n == 1)
 		oe = ie = 0;
-
+	
 	nx = mon->wx + mon->gappov*oe;
 	ny = mon->wy + mon->gappoh*oe;
 	nw = mon->ww - 2*mon->gappov*oe;
 	nh = mon->wh - 2*mon->gappoh*oe;
-
+	
 	for(i = 0, c = nexttiled(mon->clients); c; c = nexttiled(c->next)) {
 		if((i % 2 && nh / 2 > 2 * c->bw)
 		   || (!(i % 2) && nw / 2 > 2 * c->bw)) {
@@ -49,10 +47,10 @@ fibonacci(Monitor *mon, int s) {
 			}
 			if(i == 0) {
 				if(n != 1)
-					nw = mon->ww * mon->mfact;
+					nw = (mon->ww - 2*mon->gappov*oe) * mon->mfact;
 				ny = mon->wy + mon->gappoh*oe;
 			} else if(i == 1)
-				nw = mon->ww - nw;
+				nw = mon->ww - nw - 2*mon->gappov*oe;  
 			i++;
 		}
 		resize(c, nx, ny, nw - 2 * c->bw - mon->gappiv*ie,
