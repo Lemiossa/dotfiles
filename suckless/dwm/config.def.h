051819795534cb464047eccfedcd934d565768b2
Matheus Leme Da Silva <matheuslemedasilva000@gmail.com>
2026-02-15 18:53:24 -0300

    Update

--- suckless/dwm/config.def.h
+++ suckless/dwm/config.def.h
@@ -21,7 +21,7 @@ static char *colors[][3] = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2", "3", "4", "5" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -31,6 +31,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Qemu-system-i386", NULL, NULL, 0, 1, -1 }
 };
 
 /* layout(s) */
